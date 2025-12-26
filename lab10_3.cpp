fb158fdf27b5fdbb3ce1358bfda22db8ca89da49
SolidGA5 <phuphing.gh.chompubang@gmail.com>
2025-12-26 13:03:12 +0700

    #

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,31 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<fstream>
+#include<iostream>
+#include<string>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ifstream s;
+    int N = 0;
+    s.open("score.txt");
+    string text;
+    float sum = 0;
+    while (getline(s,text)){
+        sum += stof(text);
+        N++;
+    }
+    float mean = sum / N;
+    cout << "Number of data = " << N << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    float sigma = 0;
+    s.close();
+    s.open("score.txt");
+    while (getline(s,text)){
+        sigma += (stof(text)-mean)*(stof(text)-mean);
+    }
+    cout << "Standard deviation = " << sqrt((1/(float)N)*sigma);
+
+}
